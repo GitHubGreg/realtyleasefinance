6fa6ea59cc9f355a547b43868f3da80cf120ba6e
Awaken Solutions (iMac) <info@awakensolutions.com>
2024-09-27 07:55:12 -0400

    Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,10 +7,10 @@ interface LogoProps {
 export const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
     <div className={`flex flex-col items-center ${className}`}>
-      <h1 className="text-5xl font-bold tracking-tight text-neutral-900">
+      <h1 className="text-6xl font-bold tracking-tight text-neutral-900">
         RELEAF
       </h1>
-      <p className="text-md mt-0.5 border-t border-neutral-900 pt-0.5 font-medium text-neutral-600">
+      <p className="mt-0.5 border-t border-neutral-900 pt-0.5 text-sm font-medium text-neutral-600">
         Realty Lease Finance Corp
       </p>
     </div>
