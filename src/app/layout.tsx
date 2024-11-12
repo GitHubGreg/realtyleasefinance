c30bd87fddbe09c378befa8626911362a77e7e3f
Awaken Solutions (iMac) <info@awakensolutions.com>
2024-11-12 06:34:23 -0500

    Favicon fix, social share logo image and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,32 @@ export const metadata: Metadata = {
     template: '%s - RELEAF',
     default: 'RELEAF - Realty Lease Finance Corp',
   },
+  icons: {
+    icon: '/favicon.ico',
+    // If you have different sizes, you can add them like this:
+    // apple: '/apple-icon.png',
+    // shortcut: '/shortcut-icon.png',
+  },
+  description:
+    'Upfront capital to fund leasing costs, providing certainty of timely funding to tenants and brokers.',
+  openGraph: {
+    images: [
+      {
+        url: '/social-share-logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'RELEAF - Realty Lease Finance Corp',
+      },
+    ],
+    description:
+      'Upfront capital to fund leasing costs, providing certainty of timely funding to tenants and brokers.',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/social-share-logo.png'],
+    description:
+      'Upfront capital to fund leasing costs, providing certainty of timely funding to tenants and brokers.',
+  },
 }
 
 export default function RootLayout({
